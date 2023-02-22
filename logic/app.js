ac0ba88edf36d7a7d39e41bb8c34a9d9c8a15efa
Sam Bovarnick <spbovarnick@MacBook-Pro-2.attlocal.net>
2023-02-21 17:40:33 -0800

    nav working as desired

--- logic/app.js
+++ logic/app.js
@@ -1,3 +1,4 @@
+/*--------------RESUME CAROUSEL LOGIC----------------*/
 // keep track of which image is showing
 let currentPgIndx = 0;
 
@@ -32,3 +33,67 @@ function cycle(nextToPrevious) {
 
     pages[currentPgIndx].style.display = "block";
 }
+
+
+/*--------------NAVIGATION LOGIC----------------*/
+// page/section variables
+const aboutSection = document.getElementById("about");
+const resumeSection = document.getElementById("resume");
+const projectsSection = document.getElementById("projects");
+const landing = document.querySelector(".home-wrap")
+const xOut = document.getElementById("x-out");
+const bar = document.querySelector("nav");
+const navBarNav = document.querySelector("#navbarNav")
+
+// nav button variables
+const aboutButton = document.getElementById("about-button");
+const resumeButton = document.getElementById("resume-button");
+const projectsButton = document.getElementById("projects-button");
+
+
+function disappearNav() {
+    bar.style.display = "none";
+    xOut.style.display = "block";
+    landing.style.display = "none";
+}
+
+function reappearNav() {
+    bar.style.display = "block";
+    xOut.style.display = "none";
+    landing.style.display = "flex";
+}
+
+
+
+// About page nav
+aboutButton.addEventListener("click", () => {
+    disappearNav();
+    aboutSection.style.display = "block";
+
+    xOut.addEventListener("click", () => {
+        reappearNav()
+        aboutSection.style.display = "none";
+    })
+})
+
+// resume nav
+resumeButton.addEventListener("click", () => {
+    disappearNav();
+    resumeSection.style.display = "block";
+
+    xOut.addEventListener("click", () => {
+        reappearNav();
+        resumeSection.style.display = "none";
+    })
+})
+
+// projects nav
+projectsButton.addEventListener("click", () => {
+    disappearNav();
+    projectsSection.style.display = "block";
+
+    xOut.addEventListener("click", () => {
+        reappearNav();
+        projectsSection.style.display = "none";
+    })
+})
