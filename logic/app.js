eb02e213baee1133acf15456e6979270b4759fab
Sam Bovarnick <spbovarnick@MacBook-Pro-2.attlocal.net>
2023-02-24 11:07:17 -0800

    js dried and cleaned

--- logic/app.js
+++ logic/app.js
@@ -1,86 +1,44 @@
-/*--------------RESUME CAROUSEL LOGIC----------------*/
-// keep track of which image is showing
-let currentPgIndx = 0;
-
-// keep track of which page last shown
-let previousPgIdx = 0;
-
-// hold all pages with a class name "resume-pages"
-const pages = document.getElementsByClassName("resume-pages");
-
-const next = document.querySelector(".next");
-next.addEventListener('click', () => {
-    cycle(1)
-});
-
-const prev = document.querySelector(".prev");
-prev.addEventListener('click', () => {
-    cycle(-1)
-});
-
-function cycle(nextToPrevious) {
-    // set index of previous page to index of page that is currently visible
-    previousPgIdx = currentPgIndx
-    currentPgIndx = currentPgIndx + nextToPrevious
-    // hide previous page
-    pages[previousPgIdx].style.display = 'none'
-    
-    // check if next page index exists in our pages array
-    if (currentPgIndx >= pages.length) {
-        currentPgIndx = 0;
-        console.log(`hi`)
-    } else if (currentPgIndx < 0) {
-        currentPgIndx = pages.length - 1;
-    }
-    console.log(currentPgIndx)
-    console.log(pages.length)
-    console.log(previousPgIdx)
-    
-    pages[currentPgIndx].style.display = "block";
-    console.log(pages[currentPgIndx])
-
-}
-
-
 /*--------------NAVIGATION LOGIC----------------*/
-// page/section variables
+// variables
 const aboutSection = document.getElementById("about");
-// const resumeSection = document.getElementById("resume");
-// const projectsSection = document.getElementById("projects");
 const landing = document.querySelector(".home-wrap")
 const xOut = document.getElementById("x-out");
 const bar = document.querySelector("nav");
-// const navBarNav = document.querySelector("#navbarNav")
 
-// nav button variables
-// const aboutButton = document.getElementById("about-button");
-// const resumeButton = document.getElementById("resume-button");
-// const projectsButton = document.getElementById("projects-button");
+const sitePages = document.getElementsByTagName("section")
+const pageLinks = document.querySelectorAll(".nav-link")
 
-const sitePages = document.getElementById("section")
-const pageLinks = document.querySelectorAll(".nav-links")
-
+// hide navbar after navigation
 function disappearNav() {
     bar.style.display = "none";
     xOut.style.display = "flex";
     landing.style.display = "none";
 }
 
-function disappearNav() {
-    bar.style.display = "none";
-    xOut.style.display = "flex";
-    landing.style.display = "none";
+// reappear nav bar after exiting page
+function reappearNav() {
+    bar.style.display = "block";
+    xOut.style.display = "none";
+    landing.style.display = "flex";
+    landing.style.backgroundImage = "var(--main-bg-grad)"
 }
 
+// iterate through each nav link to assign event listener
 for (let i = 0; i < sitePages.length; i++) {
     pageLinks[i].addEventListener("click", () => {
+        // onclick, disappear the nav bar
         disappearNav()
+        // hide all site pages, display = "none"
         for (let j = 0; j < sitePages.length; j++){
-            pages[j].style.display = "none"
+            sitePages[j].style.display = "none"
         }
+        // show site page with same index as clicked page link, overriding line 32 for loop
         if (sitePages[i] === aboutSection) {
+            // about section needs flex rather than block display
             sitePages[i].style.display = "flex"
+            // assign x event listener
             xOut.addEventListener("click", () => {
+                // onclick, hide x, bring back navbar
                 reappearNav()
             });
         } else {
@@ -90,60 +48,7 @@ for (let i = 0; i < sitePages.length; i++) {
         });
         }
     })
-}
-
-
-// About page nav
-// navBarNav.addEventListener("click", (event) => {
-    
-//     // hide navbar & landing page, pull up the x out button
-//     function disappearNav() {
-//         bar.style.display = "none";
-//         xOut.style.display = "flex";
-//         landing.style.display = "none";
-//     }
-    
-//     // bring back nav elements and functionality & landing page
-//     function disappearNav() {
-//         bar.style.display = "none";
-//         xOut.style.display = "flex";
-//         landing.style.display = "none";
-//     }
-    
-//     disappearNav();
-    
-    
-
-//     // nav to about page
-//     if (event.target === aboutButton) {
-//         aboutSection.style.display = "flex";
-//         xOut.addEventListener("click", () => {
-//             reappearNav()
-//             aboutSection.style.display = "none"
-//         });
-//     }
-//     // nav to resume page
-//     if (event.target === resumeButton) {
-//         resumeSection.style.display = "block";
-//         xOut.addEventListener("click", () => {
-//             reappearNav()
-//             resumeSection.style.display = "none"
-//         });
-//     }
-//     // nav to projects page
-//     if (event.target === projectsButton) {
-//         projectsSection.style.display = "block";
-//         xOut.addEventListener("click", () => {
-//             reappearNav()
-//             projectsSection.style.display = "none"
-//         });
-//     }
-//     // clean up blank space clickability
-//     if (event.target !== aboutButton && event.target !== resumeButton && event.target !== projectsButton) {
-//         reappearNav();
-//     }
-// })
-
+};
 
 // change background-img gradient on landing
 landing.addEventListener("click", () => {
@@ -174,3 +79,43 @@ $(document).ready(function() {
             scroll: false  });
     });
 });
+
+/*--------------RESUME CAROUSEL LOGIC----------------*/
+// keep track of which image is showing
+let currentPgIndx = 0;
+
+// keep track of which page last shown
+let previousPgIdx = 0;
+
+// hold all pages with a class name "resume-pages"
+const pages = document.getElementsByClassName("resume-pages");
+
+const next = document.querySelector(".next");
+next.addEventListener('click', () => {
+    cycle(1)
+});
+
+const prev = document.querySelector(".prev");
+prev.addEventListener('click', () => {
+    cycle(-1)
+});
+
+function cycle(nextToPrevious) {
+    // set index of previous page to index of page that is currently visible
+    previousPgIdx = currentPgIndx
+    currentPgIndx = currentPgIndx + nextToPrevious
+    // hide previous page
+    pages[previousPgIdx].style.display = 'none'
+    
+    // check if next page index exists in our pages array
+    if (currentPgIndx >= pages.length) {
+        currentPgIndx = 0;
+    } else if (currentPgIndx < 0) {
+        // pages.length - 1, sets currentPgIdx to 1 (index of second page), since pages.length = 2.
+        currentPgIndx = pages.length - 1;
+    }
+
+
+    pages[currentPgIndx].style.display = "block";
+}
+
